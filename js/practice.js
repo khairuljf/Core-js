f3a8b717938a9ec95cfbc69e23069284c8f08238
Khairul Islam <khairuljf@gmail.com>
2018-10-19 01:20:08 +0600

    Digital clock Created

--- js/practice.js
+++ js/practice.js
@@ -542,6 +542,59 @@ dclear.addEventListener('click',function(){
 
 
 
+//clock
 
+function showTime(){
+	let date = new Date();
+	let hours = date.getHours();
+	let minutes = date.getMinutes();
+	let second = date.getSeconds()
+
+let formatHours = convertFormat(hours);
+
+hours = checkTime(hours);
+minutes = addzero(minutes);
+second = addzero(second);
+hours = addzero(hours);
+
+document.getElementById('clock').innerHTML= `${hours} : ${minutes} : ${second} ${formatHours} `;
+
+}
+
+
+function convertFormat(time){
+	let format = 'AM';
+	if(time>=12){
+
+	format = 'PM';
+	}
+	return format;
+}
+
+
+function checkTime(time){
+	if(time>12){
+		time = time -12;
+	}
+
+	if(time===0){
+		time = 12;
+	}
+
+	return time;
+}
+
+function addzero(time){
+
+	if(time<10){
+		time = "0" + time;
+	}
+
+return time;
+}
+
+
+setInterval(showTime, 1000)
+showTime();
 
 
