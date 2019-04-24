701c166879bbc1020505a3dd0d09f1b5f81779d9
khairul Islam <khairuljf@gmail.com>
2019-04-24 11:03:14 +0600

    Promise added in getPluzzle

--- js/httprequest.js
+++ js/httprequest.js
@@ -1,20 +1,18 @@
-function getPuzzle(wordCount,callback){
-        
-
+const getPuzzle = (wordCount)=> new Promise((resolve, reject)=>{
     const request = new XMLHttpRequest()
 
     request.addEventListener('readystatechange', (e)=>{
         if(e.target.readyState ===4 && e.target.status ===200){
             const data = JSON.parse(e.target.responseText)
-            callback(undefined, data.puzzle)
+            resolve(data.puzzle)
         }else if(e.target.readyState===4){
-            callback('Something woring', undefined)
+            reject('An error has taken')
         }
     })
 
     request.open('GET', `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
     request.send()
-}
+})
 
 
 function getCountryDetails(counteryCode,callbck){
