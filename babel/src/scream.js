29f0a8f4da426a32d2baa631338af2641ac574be
khairul Islam <khairuljf@gmail.com>
2019-05-16 14:21:54 +0600

    Javascrpt import export module added

---
+++ babel/src/scream.js
@@ -0,0 +1,3 @@
+
+
+export const scream = (text) => `${text.toUpperCase()}!`
---
+++ babel/src/utilities.js
@@ -0,0 +1,7 @@
+console.log('Utilities js')
+
+
+export const add = (a, b) => a+b
+
+export const name = 'khairul islam'
+
